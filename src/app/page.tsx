e0021b5ff5cde67b8c7989e7ee356e65552ceca2
James Bampoe <james.bampoe@mgb.ch>
2024-05-27 11:35:09 +0200

    feat: block main thread for some time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 import Image from "next/image";
 
-export default function Home() {
+export const dynamic = "force-dynamic";
+
+export default async function Home() {
+  /**
+   * Block the main thread for some time
+   */
+  const start = Date.now();
+  let now = start;
+  while (now - start < 5000) {
+    now = Date.now();
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
